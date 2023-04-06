decf442badd15a409cf6e5b329ad756306530ac3
Abri <abrilukumbi3@gmail.com>
2023-04-06 23:32:32 +0200

    Setting up monster fighting features

--- script.js
+++ script.js
@@ -38,6 +38,24 @@ const weapons = [
     }
 ];
 
+const monsters = [
+    {
+        name: "slime",
+        level: 2,
+        health: 15
+    },
+    {
+        name: "fanged beast",
+        level: 8,
+        health: 60
+    },
+    {
+        name: "dragon",
+        level: 20,
+        health: 300
+    }
+];
+
 const locations = [
     {
         name: "town square",
@@ -56,6 +74,12 @@ const locations = [
         "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
         "button functions": [fightSlime,fightBeast,goTown],
         text: "You enter the cave, you see some monsters."
+    }
+    {
+        name: "fight",
+        "button text": ["Attack", "Dodge", "Run"],
+        "button functions": [attack,dodge,goTown],
+        text: "You are fighting a monster."
     }
 ];
 
@@ -89,10 +113,6 @@ function goCave() {
     update(locations[2]);
 }
 
-function fightDragon() {
-    console.log("Fighting dragon.");
-}
-
 // Store page buttons functions
 
 function goTown() {
@@ -110,26 +130,69 @@ function buyHealth() {
     }
 }
 
-function buyWeapon() {
-    if (gold >= 30) {
-        gold -= 30;
-        currentWeapon++;
+function sellWeapon() {
+    if (inventory.length > 1) {
+        gold += 15;
         goldText.innerText = gold;
-        text.innerText = "You now have a new weapon."
+        let currentWeapon = inventory.shift;  // This "currentWeapon" variable is inly scoped inside this "if" statement.
+        text.innerText = "You sold a " + currentWeapon + ".";
+        text.innerText += " In your inventory you have: " + inventory;
+    } else {
+        text.innerText = "Don't sell your only weapon!";
+        text.innerText += " In your inventory you have: " + inventory;
+    }
+}
+
+function buyWeapon() {
+    if (currentWeapon < weapons.length) {
+        if (gold >= 30) {
+            gold -= 30;
+            goldText.innerText = gold;
+            currentWeapon++;
+            let newWeapon = weapons[currentWeapon].name;
+            text.innerText = "You now have a " + newWeapon + ".";
+            inventory.push(newWeapon);
+            text.innerText += " In your inventory you have: " + inventory; 
+        } else {
+            text.innerText = "You do not have enough gold to buy a weapon.";
+            text.innerText += " In your inventory you have: " + inventory;
+        }
+    } else {
+        text.innerText = "You already have the most powerful weapon.";
+        text.innerText += " In your inventory you have: " + inventory;
+        button1.innerText = "Sell weapon (15 gold)";
+        button2.onclick = sellWeapon;
     }
 }
 
 // Fight dargon page button functions
 
 function fightSlime() {
-    console.log("Going to you bought 10 health.");
+    fighting = 0;
+    goFight();
 }
 
 function fightBeast() {
-    console.log("Going to you bought 10 health.");
+    fighting = 1;
+    goFight();
 }
 
+function fightDragon() {
+    fighting = 2;
+    goFight();
+}
 
+function goFight() {
+    update(locations[3]);
+    monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = "block";   // This is how we can update CSS styles through JavaScript.
+}
 
+function attack () {
 
+}
+
+function dodge () {
+    
+}
 
